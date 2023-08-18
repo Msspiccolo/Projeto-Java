aaf611af9343b43dafaedbe6fbc6c95abc7b38f9
Msspiccolo <msspiccolo1717@gmail.com>
2023-08-17 23:47:01 -0300

    Cadastro do primeiro nome

--- src/main/java/dev/maria_stella/Main.java
+++ src/main/java/dev/maria_stella/Main.java
@@ -1,7 +1,15 @@
 package dev.maria_stella;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Digite o primeiro nome:  ");
+        Scanner Nome = new Scanner(System.in);
+        String nome = Nome.nextLine();
+        System.out.println(nome);
+        Nome.close();
+
     }
 }
