7c34f100ddef6e87cde076955c0edd0b2ac4ac3d
Msspiccolo <msspiccolo1717@gmail.com>
2023-08-17 23:51:37 -0300

    Refatorado e melhorado o  código

--- src/main/java/dev/maria_stella/Main.java
+++ src/main/java/dev/maria_stella/Main.java
@@ -2,14 +2,23 @@ package dev.maria_stella;
 
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Digite o primeiro nome:  ");
-        Scanner Nome = new Scanner(System.in);
-        String nome = Nome.nextLine();
+        Scanner sc = new Scanner(System.in);//Colocar as variáveis com nomes que mantenham o contexto
+        String nome = sc.nextLine();//Colocar a primeira palavra com letra minuscula e a segunda em diante com a primeira maiuscula
         System.out.println(nome);
-        Nome.close();
+        sc.close();
+
+
+
+
+
+
+
+
 
     }
 }
