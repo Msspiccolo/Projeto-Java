eb2ec9f68fc34b45b759ade293974bc3d7f4dcb3
Msspiccolo <msspiccolo1717@gmail.com>
2023-08-18 00:19:46 -0300

    Registrando 10 pessoas

--- src/main/java/dev/maria_stella/Main.java
+++ src/main/java/dev/maria_stella/Main.java
@@ -5,21 +5,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Digite o primeiro nome:  ");
         Scanner sc = new Scanner(System.in);//Colocar as variáveis com nomes que mantenham o contexto
-        String nome = sc.nextLine();//Colocar a primeira palavra com letra minuscula e a segunda em diante com a primeira maiuscula
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println("Digite o primeiro nome:  ");
+            String nome = sc.nextLine();//Colocar a primeira palavra com letra minuscula e a segunda em diante com a primeira maiuscula
 
-        System.out.println("Digite o seu CPF:  ");
-        String cpf = sc.nextLine();
+            System.out.println("Digite o seu CPF:  ");
+            String cpf = sc.nextLine();
 
-        System.out.println("Digite a sua data de Nascimento:  ");
-        String dataNascimento = sc.nextLine();
+            System.out.println("Digite a sua data de Nascimento:  ");
+            String dataNascimento = sc.nextLine();
 
-        Pessoa pessoa  = new Pessoa(nome,dataNascimento,cpf);
-        System.out.println(pessoa.getNome());
-        System.out.println(pessoa.getCpf());
-        System.out.println(pessoa.getDataNascimento());
-
+            Pessoa pessoa = new Pessoa(nome, dataNascimento, cpf);
+            System.out.println(pessoa.getNome());
+            System.out.println(pessoa.getCpf());
+            System.out.println(pessoa.getDataNascimento());
+        }
         sc.close();
 
 
