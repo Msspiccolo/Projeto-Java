ba32c7d162f330b176422f10c1c5353e26c54c04
Msspiccolo <msspiccolo1717@gmail.com>
2023-08-17 23:57:29 -0300

    Cadastrando informações do usuário

--- src/main/java/dev/maria_stella/Main.java
+++ src/main/java/dev/maria_stella/Main.java
@@ -9,9 +9,17 @@ public class Main {
         System.out.println("Digite o primeiro nome:  ");
         Scanner sc = new Scanner(System.in);//Colocar as variáveis com nomes que mantenham o contexto
         String nome = sc.nextLine();//Colocar a primeira palavra com letra minuscula e a segunda em diante com a primeira maiuscula
-        System.out.println(nome);
-        sc.close();
 
+        System.out.println("Digite o seu CPF:  ");
+        String cpf = sc.nextLine();
+
+        System.out.println("Digite a sua data de Nascimento:  ");
+        String dataNascimento = sc.nextLine();
+
+        System.out.println(nome);
+        System.out.println(cpf);
+        System.out.println(dataNascimento);
+        sc.close();
 
 
 
