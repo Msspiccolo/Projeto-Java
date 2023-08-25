a5e99c4c750a303a756aa77ae13c94735418ab9d
João Guilherme <jaaumgui@gmail.com>
2023-08-25 01:00:48 -0300

    Resolvido bug do scanner

--- src/main/java/dev/maria_stella/Main.java
+++ src/main/java/dev/maria_stella/Main.java
@@ -8,11 +8,19 @@ import java.util.Scanner;
 
 public class Main {
     //Metodo estatico permite executa-lo sem instanciar a classe
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);//Colocar as variáveis com nomes que mantenham o contexto
-        int opcao = menu();
 
+    /**
+     *
+     * Como o scanner está sendo utilizado ao longo dos métodos, transformei ele em um atributo da classe Main
+     * Assim o scanner só é instanciado uma vez e pode ser utilizado em todos os métodos
+     *
+     **/
+    private static final Scanner sc = new Scanner(System.in);//Colocar as variáveis com nomes que mantenham o contexto
+
+
+    public static void main(String[] args) {
         List<Pessoa> pessoas = new ArrayList<>();
+        int opcao = menu();
         while (opcao != 5) {
             switch (opcao) {
                 case 1 -> {
@@ -25,50 +33,32 @@ public class Main {
                     opcao = menu();
                 }
                 case 2 -> {
-                    for (Pessoa pessoa : pessoas) {
+                    for (Pessoa pessoa : pessoas) { //Mesma coisa que o "for(int i = 0; i< pessoas.size(); i++)"
                         System.out.println(pessoa);
                     }
                     opcao = menu();
                 }
             }
         }
-
-
         sc.close();
-
-
-
-
-
-
-
     }
 
     public static Pessoa cadastrarPessoa(){
-        Scanner sc = new Scanner(System.in);
         Endereco endereco = cadastrarEndereco();
         System.out.println("Digite o primeiro nome:  ");
         String nome = sc.nextLine();//Colocar a primeira palavra com letra minuscula e a segunda em diante com a primeira maiuscula
-
         System.out.println("Digite o seu CPF:  ");
         String cpf = sc.nextLine();
-
         System.out.println("Digite a sua data de Nascimento:  ");
         LocalDate dataNascimento = LocalDate.parse(sc.nextLine(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-
         System.out.println("Digite o seu E-mail:  ");
         String email = sc.nextLine();
-
         System.out.println("Digite o seu Telefone:  ");
         String telefone = sc.nextLine();
-
-        sc.close();
-
         return new Pessoa (nome, dataNascimento, cpf,email,telefone,endereco);
     }
 
     public static Endereco cadastrarEndereco(){
-        Scanner sc = new Scanner(System.in);
         System.out.println("Digite o nome da sua rua:  ");
         String rua = sc.nextLine();
         System.out.println("Digite o nome do seu bairro:  ");
@@ -83,23 +73,17 @@ public class Main {
         String numCasa = sc.nextLine();
         System.out.println("Digite o complemento:  ");
         String complemento = sc.nextLine();
-        sc.close();
         return new Endereco(rua,bairro,cidade,numCasa,complemento,estado,cep);
     }
 
 
     public static int menu(){
-        Scanner sc = new Scanner(System.in);
-        int opcao = 0;
-        System.out.println("Digite 1 para Cadastrar pessoa:   ");
-        System.out.println("Digite 2 para Alterar pessoa:   ");
-        System.out.println("Digite 3 para Excluir pessoa:   ");
-        System.out.println("Digite 4 para Ler pessoa:   ");
+        System.out.println("Digite 1 para Cadastrar uma nova pessoa:   ");
+        System.out.println("Digite 2 para Ver todas as pessoas:   ");
+        System.out.println("Digite 3 para Alterar uma pessoa:   ");
+        System.out.println("Digite 4 para Excluir uma pessoa:   ");
         System.out.println("Digite 5 para Sair:    ");
-        opcao = sc.nextInt();
-        sc.close();
-        return opcao;
-
+        return sc.nextInt();
     }
 
 
