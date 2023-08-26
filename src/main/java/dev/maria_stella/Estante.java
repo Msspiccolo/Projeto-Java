00aca2ae0a7ea5a7006c2e003d54f149a1e87aff
Msspiccolo <msspiccolo1717@gmail.com>
2023-08-26 00:26:14 -0300

    criado classe estante

---
+++ src/main/java/dev/maria_stella/Estante.java
@@ -0,0 +1,73 @@
+package dev.maria_stella;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Estante {
+    private List<Livro> livros;
+
+    public Estante() {
+        livros = new ArrayList<>();
+    }
+
+    public Estante(List<Livro> livros) {
+        this.livros = livros;
+    }
+
+    public Livro escolheLivro(int posicao) {
+        Livro livro = livros.get(posicao);
+        removeLivro(livro);
+        return livro;
+
+    }
+
+    public Livro escolheLivro(String nome) {
+        for (int i = 0; i < livros.size(); i++) {
+            Livro livro = livros.get(i);
+            if (livro.getTitulo().equals(nome)) {
+                removeLivro(livro);
+                return livro;
+            }
+        }
+        return null;
+    }
+
+    public void colocarLivro(Livro livro) {
+        livros.add(livro);
+
+    }
+
+
+    public void removeLivro(Livro escolheLivro) {
+        livros.remove(escolheLivro);
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Estante estante = (Estante) o;
+        return Objects.equals(livros, estante.livros);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(livros);
+    }
+
+    @Override
+    public String toString() {
+        return "Estante{" +
+                "livros=" + livros +
+                '}';
+    }
+}
